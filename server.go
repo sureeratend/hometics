b2a0b948ed1e31649d4dea9d0b3fda2385f9b3ba
sureerat <>
2020-09-27 16:02:42 +0700

    Insert database

--- server.go
+++ server.go
@@ -12,11 +12,6 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type Pair struct {
-	DeviceID int64
-	UserID   int64
-}
-
 // git init
 // git add server.go go.mod
 // git commit -m "[N] Initial project"
@@ -37,40 +32,34 @@ func main() {
 	log.Fatal(server.ListenAndServe())
 }
 
+type Pair struct {
+	DeviceID int64
+	UserID   int64
+}
+
 func PairDeviceHandler(w http.ResponseWriter, r *http.Request) {
 	var p Pair
 	err := json.NewDecoder(r.Body).Decode(&p)
 	if err != nil {
-		fmt.Printf("fdsjkfghsd\n")
 		w.WriteHeader(http.StatusBadRequest)
 		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
+
 	defer r.Body.Close()
+
 	fmt.Printf("pair: %#v\n", p)
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-	//db, err := sql.Open("postgres", "postgres://cfpbmgmh:4RM8d4XhNM9zD3GqjSPp5K9e7REh7STF@satao.db.elephantsql.com:5432/cfpbmgmh")
 
+	db, err := sql.Open("postgress", os.Getenv("DATABASE_URL"))
 	if err != nil {
-		log.Fatal("connetc to database fail error", err, os.Getenv("DATABASE_URL"))
+		log.Fatal(err)
 	}
-	defer db.Close()
 
-	createTb := `CREATE TABLE IF NOT EXISTS pair( DEVICE_ID INTEGER NOT NULL, USER_ID INTEGER NOT NULL);`
-
-	_, err = db.Exec(createTb)
+	_, err = db.Exec("INSERT INTO pairs VALUES ($1,$2)", p.DeviceID, p.UserID)
 	if err != nil {
-		log.Fatal("cant 't connect to database err:", err)
-	}
-	fmt.Println("Create database success")
-
-	_, err = db.Exec("INSERT INTO pairs VALUES($1,$2);", p.DeviceID, p.UserID)
-	if err != nil {
-		fmt.Printf("fdsjkfghsd\n")
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
-
 	w.Write([]byte(`{"status":"active"}`))
 }
